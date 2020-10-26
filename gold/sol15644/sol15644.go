4468d2692bbc0169aa7d379967f17022e4611569
Yoel-18 <63850050+Yoel-18@users.noreply.github.com>
2020-10-27 00:13:23 +0900

    S

---
+++ gold/sol15644/sol15644.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var (
+	br                               = bufio.NewReader(os.Stdin)
+	bw                               = bufio.NewWriter(os.Stdout)
+	n, m, count                      int
+	board                            [][]string
+	redX, redY, blueX, blueY, hX, hY int
+	dPos                             = []point{{0, 1, "U"}, {0, -1, "D"}, {1, 0, "L"}, {-1, 0, "R"}}
+	check                            [10][10][10][10]bool
+	countStr                         string
+)
+
+func main() {
+	defer bw.Flush()
+	fmt.Fscan(br, &n, &m)
+	board = make([][]string, n)
+	for i := 0; i < n; i++ {
+		board[i] = make([]string, m)
+		var str string
+		fmt.Fscan(br, &str)
+		board[i] = strings.Split(str, "")
+		for j := 0; j < m; j++ {
+			if board[i][j] == "R" {
+				redX, redY = i, j
+			}
+			if board[i][j] == "B" {
+				blueX, blueY = i, j
+			}
+			if board[i][j] == "O" {
+				hX, hY = i, j
+			}
+		}
+	}
+	find()
+	fmt.Fprintln(bw, count)
+	fmt.Fprintln(bw, countStr)
+
+}
+func find() {
+	q := make([][5]int, 0)
+	q = append(q, [5]int{redX, redY, blueX, blueY, 0})
+	qStr := make([]string, 0)
+	qStr = append(qStr, "")
+	check[redX][redY][blueX][blueY] = true
+	for len(q) > 0 {
+		rx, ry, bx, by, dis := q[0][0], q[0][1], q[0][2], q[0][3], q[0][4]
+		q = q[1:]
+		str := qStr[0]
+		qStr = qStr[1:]
+		for i := 0; i < 4; i++ {
+			newRX, newRY, newBX, newBY, newDis := rx, ry, bx, by, dis+1
+			countR, countB := 0, 0
+			newStr := str
+			for board[newRX+dPos[i].x][newRY+dPos[i].y] != "#" && board[newRX][newRY] != "O" {
+				newRX += dPos[i].x
+				newRY += dPos[i].y
+				countR++
+			}
+			for board[newBX+dPos[i].x][newBY+dPos[i].y] != "#" && board[newBX][newBY] != "O" {
+				newBX += dPos[i].x
+				newBY += dPos[i].y
+				countB++
+			}
+			if newBX == hX && newBY == hY {
+				continue
+			}
+			if newRX == hX && newRY == hY {
+				count = newDis
+				countStr = newStr
+				return
+			}
+			if newRX == newBX && newRY == newBY {
+				if countR > countB {
+					newRX -= dPos[i].x
+					newRY -= dPos[i].y
+				} else {
+					newBX -= dPos[i].x
+					newBY -= dPos[i].y
+				}
+			}
+			if !check[newRX][newRY][newBX][newBY] {
+				check[newRX][newRY][newBX][newBY] = true
+				q = append(q, [5]int{newRX, newRY, newBX, newBY, newDis})
+			}
+		}
+	}
+	count = -1
+}
+
+type point struct {
+	x, y int
+	pos  string
+}
